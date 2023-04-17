7fc274687aafcbee55ed6c5aff65bdee5bcc5d74
syunraii <syunraii@outlook.com>
2023-04-17 17:50:27 +0800

    test

--- Module/AbemaTv check/abema_region_bypass.user.js
+++ Module/AbemaTv check/abema_region_bypass.user.js
@@ -1,12 +1,14 @@
-// ==UserScript==
-// @name Abema 区域限制
-// @author https://gist.github.com/Last-Order/5a1c70686bc0c130fe3fbdcbdb1bf22a
-// @version 1.0.0
-// @run-at document-end
-// @namespace Violentmonkey Scripts
-// @match https://abema.tv/*
-// @grant none
-// ==/UserScript==
+/*
+    // ==UserScript==
+    // @name Abema 区域限制
+    // @author https://gist.github.com/Last-Order/5a1c70686bc0c130fe3fbdcbdb1bf22a
+    // @version 1.0.0
+    // @run-at document-end
+    // @namespace Violentmonkey Scripts
+    // @match https://abema.tv/*
+    // @grant none
+    // ==/UserScript==
+*/
 Object.defineProperty(__CLIENT_REGION__, 'isAllowed', {
     get: () => true
 });
